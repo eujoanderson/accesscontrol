7d2bf4e70b43b0851a795f8fe050d3b6100a4bc2
Joanderson <joandersonsantossouza09@gmail.com>
2023-10-06 01:03:54 -0300

    teste

--- js/main.js
+++ js/main.js
@@ -1,8 +1,8 @@
 import requisicao from './ajax.js';
 import './script.js';
 import '../css/page-principal.css';
-import '../css/style.css'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.css'
+import '../css/style.css';
+import 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.css'; 
 
 window.requisicaoPaginas = requisicao.requisicaoPaginas;
