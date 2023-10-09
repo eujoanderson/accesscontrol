44233ca96f449e095af1a98cebcd49b65c174332
Joanderson <joandersonsantossouza09@gmail.com>
2023-10-09 20:30:09 -0300

    Implementando o json-server

--- js/component/personTable.js
+++ js/component/personTable.js
@@ -8,9 +8,9 @@ function personTable (host){
 		<td>${host.name}</td>
 		<td>${host.card}</td>
 		<td>${host.setor}</td>
-		<td class="icon-container">
-			<a class="icon-edit"><i class='bx bxs-pencil' ></i></a>
-			<a class=" icon-edit" ><i class='bx bx-x' ></i></a> 
+		<td class="icon-container ">
+			<a class="icon-edit" style="cursor:pointer;"><i class='bx bxs-pencil' ></i></a>
+			<a class=" icon-edit" style="cursor:pointer;"><i class='bx bx-x' ></i></a> 
 		</td>
 
     </tr>
